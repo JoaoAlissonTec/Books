b2f84db8742db7c6560fff2c789e34c057c459dc
João Alisson <76262867+JoaoAlissonTec@users.noreply.github.com>
2024-07-01 11:01:19 -0300

    :recycle: refactor: Dotenv para manter confidencial os dados sensíveis

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose")
 
+require("dotenv").config()
+
 async function main(){
     try{
         await mongoose.connect(
-            "mongodb+srv://joaoalisson222005:13242526Ab@cluster0.y90yeio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@cluster0.y90yeio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
         )
         console.log("Conectado ao banco")
     }catch(error){
